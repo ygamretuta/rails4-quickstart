36f33564e1e13453e49168c6c9dfdc00ef9e14bc
ygamretuta <ygam.j.retuta@gmail.com>
2013-09-08 21:08:11 +0800

    add book to seeds file, fix show, index on books

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,8 @@ class Book < ActiveRecord::Base
   belongs_to :seller, :class_name => 'User', :foreign_key => :seller_id
   mount_uploader :photo, PhotoUploader
 
-  attr_accessible :buyer, :seller, :title, :photo, :price
+  attr_accessible :buyer_id, :seller_id, :title, :photo, :price
   paginates_per 1
 
-  validates_presence_of :buyer, :seller
+  validates_associated :buyer, :seller
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create({email:'user1@example.com', password:'12345678', password_confirmation:'12345678'})
-User.create({email:'user2@example.com', password:'12345678', password_confirmation:'12345678'})
+u1 = User.create({email:'user1@example.com', password:'12345678', password_confirmation:'12345678'})
+u2 = User.create({email:'user2@example.com', password:'12345678', password_confirmation:'12345678'})
+b1 = Book.new({title:'Harry Potter', price:34.99})
+b1.buyer = u1
+b1.seller = u2
+b1.save!
+b2 = Book.new({title:'The Road', price:5.99})
+b2.buyer = u2
+b2.seller = u1
+b2.save!
